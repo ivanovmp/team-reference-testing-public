bd7cd972c5ca2b7c56e5611402bcd99addfc683f
ivanovmp <ivanovmihail99@gmail.com>
2023-07-11 17:30:50 +0300

    Fix typo

--- palindromes/testing/Timus.1297 - Palindromes/post.cpp
+++ palindromes/testing/Timus.1297 - Palindromes/post.cpp
@@ -6,7 +6,7 @@ int main() {
     pii p{};
     for (int i = 0; i < s.size(); ++i) {
         if (o[i] * 2 - 1 > p.y - p.x) p = {i - o[i] + 1, i + o[i]};
-        if (e[i] * 2 > p.y - p.x) p = {i - o[i], i + o[i]};
+        if (e[i] * 2 > p.y - p.x) p = {i - e[i], i + e[i]};
     }
     cout << s.substr(p.x, p.y - p.x);
 }
