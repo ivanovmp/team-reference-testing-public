1106498546fc7c3e56da1c6d3a0feb2e92239dca
ivanovmp <ivanovmihail99@gmail.com>
2023-07-11 17:28:40 +0300

    Include cstdint, fix solution

--- palindromes/testing/Timus.1297 - Palindromes/post.cpp
+++ palindromes/testing/Timus.1297 - Palindromes/post.cpp
@@ -5,8 +5,8 @@ int main() {
        e = build_even_palindromes(s);
     pii p{};
     for (int i = 0; i < s.size(); ++i) {
-        if (o[i] > p.y - p.x) p = {i - o[i] + 1, i + o[i]};
-        if (e[i] > p.y - p.x) p = {i - o[i], i + o[i]};
+        if (o[i] * 2 - 1 > p.y - p.x) p = {i - o[i] + 1, i + o[i]};
+        if (e[i] * 2 > p.y - p.x) p = {i - o[i], i + o[i]};
     }
     cout << s.substr(p.x, p.y - p.x);
 }
