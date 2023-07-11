45e76781bcf2245bcd61f3d1d83d54afcfadce9d
ivanovmp <ivanovmihail99@gmail.com>
2023-07-11 17:18:40 +0300

    Add temp directory

--- palindromes/testing/Timus.1297 - Palindromes/test.sh
+++ palindromes/testing/Timus.1297 - Palindromes/test.sh
@@ -1,2 +1,3 @@
+mkdir ./temp
 cat ./utils/typedefs.hpp ./palindromes/manacher.cpp "./palindromes/testing/Timus.1297 - Palindromes/post.cpp" > ./temp/code.cpp
 python3 ./utils/online_judges_benchmark.py ./temp/code.cpp timus 1297 -t Accepted
