ed15478dc41c7f72d9724f034ad5831bd6fe9f63
ivanovmp <ivanovmihail99@gmail.com>
2023-07-22 20:17:41 +0300

    Update prepare-firefox.sh

--- utils/firefox/prepare-firefox.sh
+++ utils/firefox/prepare-firefox.sh
@@ -1,5 +1,5 @@
 sudo apt remove firefox
 sudo add-apt-repository ppa:mozillateam/ppa
-sudo sh utils/alter-firefox-package-priority.sh
+sudo sh utils/firefox/alter-firefox-package-priority.sh
 sudo apt install firefox -y
 pip install orjson selenium-firefox webdriver-manager
