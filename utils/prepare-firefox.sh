ea9138d28357383b703813528a4a36eb0c4d173d
ivanovmp <ivanovmihail99@gmail.com>
2023-07-22 20:15:09 +0300

    Update prepare-firefox.sh

--- utils/prepare-firefox.sh
+++ utils/prepare-firefox.sh
@@ -2,11 +2,7 @@ sudo apt remove firefox
 
 sudo add-apt-repository ppa:mozillateam/ppa
 
-echo '
-Package: *
-Pin: release o=LP-PPA-mozillateam
-Pin-Priority: 1001
-' | sudo tee /etc/apt/preferences.d/mozilla-firefox
+sudo sh utils/alter-firefox-package-priority.sh
 
 sudo apt install firefox -y
 
