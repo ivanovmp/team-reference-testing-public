4e5bb24cf9a331e2fd5d0a553f16c92c21f8f197
ivanovmp <ivanovmihail99@gmail.com>
2023-07-22 21:08:21 +0300

    Add a comment in Dinic

--- dinic/testing/marbles/test.sh
+++ dinic/testing/marbles/test.sh
@@ -1,3 +1,3 @@
 mkdir ./temp
-cat ./dinic/testing/marbles/pre.cpp ./dinic/flow.hpp ./dinic/testing/marbles/post.cpp > ./temp/code.cpp
+cat date ./dinic/testing/marbles/pre.cpp ./dinic/flow.hpp ./dinic/testing/marbles/post.cpp > ./temp/code.cpp
 python3 ./utils/online_judges_benchmark.py ./temp/code.cpp codeforces I -c 103855 -s gym -t OK
