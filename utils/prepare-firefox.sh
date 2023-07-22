fed0cd945270ad5aefa2113f79a8a22bc991235d
ivanovmp <ivanovmihail99@gmail.com>
2023-07-22 20:06:05 +0300

    Create prepare-firefox.sh

---
+++ utils/prepare-firefox.sh
@@ -0,0 +1,14 @@
+sudo apt remove firefox
+
+sudo add-apt-repository ppa:mozillateam/ppa
+
+echo '
+Package: *
+Pin: release o=LP-PPA-mozillateam
+Pin-Priority: 1001
+' | sudo tee /etc/apt/preferences.d/mozilla-firefox
+
+sudo apt install firefox -y
+
+pip install orjson selenium-firefox webdriver-manager
+
