c9ab00a9c92b270936fcb7a2a9bf2929635eab43
ivanovmp <ivanovmihail99@gmail.com>
2023-07-28 00:12:40 +0300

    Add declaration of suffix array

--- burrows_wheeler/testing/Timus.1322. Spy/test.sh
+++ burrows_wheeler/testing/Timus.1322. Spy/test.sh
@@ -1,3 +1,3 @@
 mkdir ./temp
-cat "./burrows_wheeler/testing/Timus.1322. Spy/pre.cpp" ./utils/typedefs.hpp ./burrows_wheeler/burrows_wheeler.hpp "./burrows_wheeler/testing/Timus.1322. Spy/post.cpp" > ./temp/code.cpp
+cat "./burrows_wheeler/testing/Timus.1322. Spy/pre.cpp" ./utils/typedefs.hpp "./burrows_wheeler/testing/Timus.1322. Spy/inter.cpp" ./burrows_wheeler/burrows_wheeler.hpp "./burrows_wheeler/testing/Timus.1322. Spy/post.cpp" > ./temp/code.cpp
 python3 ./utils/online_judges_benchmark.py ./temp/code.cpp timus 1322 -t Accepted
