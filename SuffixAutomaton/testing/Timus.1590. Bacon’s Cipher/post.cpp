0511e7f87098fdb0be4c2b95d33313b6473daefb
ivanovmp <ivanovmihail99@gmail.com>
2023-07-27 20:16:35 +0300

    Add Timus.1590 Suffix Automaton CI

--- SuffixAutomaton/testing/Timus.1590. Bacon’s Cipher/post.cpp
+++ SuffixAutomaton/testing/Timus.1590. Bacon’s Cipher/post.cpp
@@ -1,9 +1,16 @@
 int main() {
     string s;
     cin >> s;
-    vi suf = build_suffix_array(s, 'z' + 1);
-    vi lcp = build_lcp(s, suf);
-    ll ans = s.size() * (s.size() + 1ll) / 2;
-    for (int i : lcp) ans -= i;
-    cout << ans << '\n';
+    vi a(s.begin(), s.end());
+    for (int& i : a) if (i < 0) i += 256;
+    const int ALPHABET = 256;
+    suffix::Automaton automaton(a, ALPHABET);
+    vi ans(automaton.quant, 1);
+    for (int i = automaton.quant - 1; i >= 0; --i)
+        for (int j = 0; j < ALPHABET; ++j) {
+            int to = automaton.next_step[j][i];
+            if (to != -1)
+                ans[i] += ans[to];
+        }
+    cout << ans[0] - 1 << '\n';
 }
