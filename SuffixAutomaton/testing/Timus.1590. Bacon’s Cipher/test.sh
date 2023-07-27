84f4840f945757efa7193d6df929462449b288b2
ivanovmp <ivanovmihail99@gmail.com>
2023-07-27 20:16:35 +0300

    Add Timus.1590 Suffix Automaton CI

--- SuffixAutomaton/testing/Timus.1590. Bacon’s Cipher/test.sh
+++ SuffixAutomaton/testing/Timus.1590. Bacon’s Cipher/test.sh
@@ -1,3 +1,3 @@
 mkdir ./temp
-cat "./suffix_array/testing/Timus.1590. Bacon’s Cipher/pre.cpp" ./utils/typedefs.hpp ./suffix_array/suffix_array.hpp "./suffix_array/testing/Timus.1590. Bacon’s Cipher/post.cpp" > ./temp/code.cpp
+cat "./SuffixAutomaton/testing/Timus.1590. Bacon’s Cipher/pre.cpp" ./utils/typedefs.hpp ./SuffixAutomaton/suffix_automaton.hpp "./SuffixAutomaton/testing/Timus.1590. Bacon’s Cipher/post.cpp" > ./temp/code.cpp
 python3 ./utils/online_judges_benchmark.py ./temp/code.cpp timus 1590 -t Accepted
