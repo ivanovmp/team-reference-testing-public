ac34139a11e19d7b2a3ed9834eeacc864a0b1f63
ivanovmp <ivanovmihail99@gmail.com>
2023-07-28 00:09:47 +0300

    Fix usage of suffix array

--- suffix_array/testing/Timus.1590. Bacon’s Cipher/post.cpp
+++ suffix_array/testing/Timus.1590. Bacon’s Cipher/post.cpp
@@ -1,8 +1,8 @@
 int main() {
     string s;
     cin >> s;
-    vi suf = build_suffix_array<uint8_t>(s, 'z' + 1);
-    vi lcp = build_lcp(s, suf);
+    vi suf = build_suffix_array<uint8_t>(s.begin(), s.end(), 'z' + 1);
+    vi lcp = build_lcp(s.begin(), s.end(), suf);
     ll ans = s.size() * (s.size() + 1ll) / 2;
     for (int i : lcp) ans -= i;
     cout << ans << '\n';
