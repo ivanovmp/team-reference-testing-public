1d6ed0c836a4748837f67546ac952fbdab56b203
ivanovmp <ivanovmihail99@gmail.com>
2024-02-27 04:19:38 +0300

    Fix powmod usage

--- FFT/testing/test/main.cpp
+++ FFT/testing/test/main.cpp
@@ -317,10 +317,10 @@ TEST(FPS, A000272_A001858) {
 TEST(NTT, CorrectPowersTest) {
     ASSERT_TRUE(NT::is_prime(ntt.MOD));
     ASSERT_EQ(1, ntt.MOD & (1 << ntt.n) - 1);
-    ASSERT_EQ(ntt.ROOT, NT::binpow(ntt.G, (ntt.MOD - 1) >> ntt.n, ntt.MOD));
-    ASSERT_EQ(1, NT::binpow(ntt.ROOT, 1 << ntt.n, ntt.MOD));
-    ASSERT_NE(1, NT::binpow(ntt.ROOT, 1 << ntt.n - 1, ntt.MOD));
-    ASSERT_EQ(ntt.MOD - 1, NT::binpow(ntt.imaginary_unit, 2, ntt.MOD));
+    ASSERT_EQ(ntt.ROOT, NT::powmod(ntt.G, (ntt.MOD - 1) >> ntt.n, ntt.MOD));
+    ASSERT_EQ(1, NT::powmod(ntt.ROOT, 1 << ntt.n, ntt.MOD));
+    ASSERT_NE(1, NT::powmod(ntt.ROOT, 1 << ntt.n - 1, ntt.MOD));
+    ASSERT_EQ(ntt.MOD - 1, NT::powmod(ntt.imaginary_unit, 2, ntt.MOD));
 }
 
 const int NTT_CORRECT_ANS_TEST_SIZE = 1500;
