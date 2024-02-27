1fbbe34cd02971c2018bea2eafb34304b231b7a8
ivanovmp <ivanovmihail99@gmail.com>
2024-02-27 03:51:27 +0300

    Include modulo.hpp instead of NT.hpp in Timus.1996. Cipher Message 3

--- FFT/testing/Timus.1996. Cipher Message 3/test.sh
+++ FFT/testing/Timus.1996. Cipher Message 3/test.sh
@@ -1,6 +1,6 @@
 mkdir ./temp
 cat "./FFT/testing/Timus.1996. Cipher Message 3/pre_fft.cpp" ./utils/typedefs.hpp ./basic_strings/z_function.hpp ./FFT/FFT.hpp "./FFT/testing/Timus.1996. Cipher Message 3/post_fft.cpp" > ./temp/code_fft.cpp
-cat "./FFT/testing/Timus.1996. Cipher Message 3/pre_ntt.cpp" ./utils/typedefs.hpp ./basic_strings/z_function.hpp ./number_theory/NT.hpp ./FFT/NTT.hpp "./FFT/testing/Timus.1996. Cipher Message 3/post_ntt.cpp" > ./temp/code_ntt.cpp
+cat "./FFT/testing/Timus.1996. Cipher Message 3/pre_ntt.cpp" ./utils/typedefs.hpp ./basic_strings/z_function.hpp ./number_theory/modulo§.hpp ./FFT/NTT.hpp "./FFT/testing/Timus.1996. Cipher Message 3/post_ntt.cpp" > ./temp/code_ntt.cpp
 echo "Trying FFT:"
 python3 ./utils/online_judges_benchmark.py ./temp/code_fft.cpp timus 1996 -t Accepted
 echo "Trying NTT:"
